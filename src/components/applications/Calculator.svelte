42d3771df0e25b08c3d6af4f55f77d9b37d2436b
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2019-08-13 17:11:30 +0700

    add calculator app

--- src/components/applications/Calculator.svelte
+++ src/components/applications/Calculator.svelte
@@ -2,6 +2,139 @@
   export let val;
   import Guard from "../layouts/Guard.svelte";
   import Workspace from "../layouts/Workspace.svelte";
+
+  let calculator;
+  let displayedNum = 0;
+
+  const calculate = (n1, operator, n2) => {
+    const firstNum = parseFloat(n1);
+    const secondNum = parseFloat(n2);
+    if (operator === "add") return firstNum + secondNum;
+    if (operator === "subtract") return firstNum - secondNum;
+    if (operator === "multiply") return firstNum * secondNum;
+    if (operator === "divide") return firstNum / secondNum;
+  };
+
+  const getKeyType = key => {
+    const { action } = key.dataset;
+    if (!action) return "number";
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    )
+      return "operator";
+    // For everything else, return the action
+    return action;
+  };
+
+  const createResultString = (key, displayedNum, state) => {
+    const keyContent = key.textContent;
+    const keyType = getKeyType(key);
+    const { firstValue, operator, modValue, previousKeyType } = state;
+
+    if (keyType === "number") {
+      return displayedNum === 0 ||
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+        ? keyContent
+        : displayedNum + keyContent;
+    }
+
+    if (keyType === "decimal") {
+      if (!displayedNum.includes(".")) return displayedNum + ".";
+      if (previousKeyType === "operator" || previousKeyType === "calculate")
+        return "0.";
+      return displayedNum;
+    }
+
+    if (keyType === "operator") {
+      return firstValue &&
+        operator &&
+        previousKeyType !== "operator" &&
+        previousKeyType !== "calculate"
+        ? calculate(firstValue, operator, displayedNum)
+        : displayedNum;
+    }
+
+    if (keyType === "clear") return 0;
+
+    if (keyType === "calculate") {
+      return firstValue
+        ? previousKeyType === "calculate"
+          ? calculate(displayedNum, operator, modValue)
+          : calculate(firstValue, operator, displayedNum)
+        : displayedNum;
+    }
+  };
+
+  const updateCalculatorState = (
+    key,
+    calculator,
+    calculatedValue,
+    displayedNum
+  ) => {
+    const keyType = getKeyType(key);
+    const {
+      firstValue,
+      operator,
+      modValue,
+      previousKeyType
+    } = calculator.dataset;
+
+    calculator.dataset.previousKeyType = keyType;
+
+    if (keyType === "operator") {
+      calculator.dataset.operator = key.dataset.action;
+      calculator.dataset.firstValue =
+        firstValue &&
+        operator &&
+        previousKeyType !== "operator" &&
+        previousKeyType !== "calculate"
+          ? calculatedValue
+          : displayedNum;
+    }
+
+    if (keyType === "calculate") {
+      calculator.dataset.modValue =
+        firstValue && previousKeyType === "calculate" ? modValue : displayedNum;
+    }
+
+    if (keyType === "clear" && key.textContent === "AC") {
+      calculator.dataset.firstValue = "";
+      calculator.dataset.modValue = "";
+      calculator.dataset.operator = "";
+      calculator.dataset.previousKeyType = "";
+    }
+  };
+
+  const updateVisualState = (key, calculator) => {
+    const keyType = getKeyType(key);
+    Array.from(key.parentNode.children).forEach(k =>
+      k.classList.remove("is-depressed")
+    );
+
+    if (keyType === "operator") key.classList.add("is-depressed");
+    if (keyType === "clear" && key.textContent !== "AC") key.textContent = "AC";
+    if (keyType !== "clear") {
+      const clearButton = calculator.querySelector("[data-action=clear]");
+      clearButton.textContent = "CE";
+    }
+  };
+
+  const getStroke = e => {
+    const key = e.target;
+    const resultString = createResultString(
+      key,
+      displayedNum,
+      calculator.dataset
+    );
+
+    displayedNum = resultString;
+    updateCalculatorState(key, calculator, resultString, displayedNum);
+    updateVisualState(key, calculator);
+  };
 </script>
 
 <style>
@@ -64,9 +197,9 @@
 
 <Guard {val}>
   <Workspace id="calculator-app">
-    <div class="calculator">
-      <div class="calculator__display">0</div>
-      <div class="calculator__keys">
+    <div class="calculator" bind:this={calculator}>
+      <div class="calculator__display">{displayedNum}</div>
+      <div class="calculator__keys" on:click={getStroke}>
         <button class="key--operator" data-action="add">+</button>
         <button class="key--operator" data-action="subtract">-</button>
         <button class="key--operator" data-action="multiply">&times;</button>
